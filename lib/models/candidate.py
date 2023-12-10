e4c359096296c92bc3a053b82ab5a40cc72fb277
Ej Myles <ejelymyles@gmail.com>
2023-12-10 01:45:10 -0800

    add find_by_id to candidate class

--- lib/models/candidate.py
+++ lib/models/candidate.py
@@ -163,3 +163,15 @@ class Candidate:
          """
          rows = CURSOR.execute(sql).fetchall()
          return [cls.instance_from_db(row) for row in rows]
+
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * 
+            FROM candidates
+            WHERE id = ?
+         """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+        
