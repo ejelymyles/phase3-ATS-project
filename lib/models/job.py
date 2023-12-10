099ea83cf0812fbf58303bc80efa47567527c956
Ej Myles <ejelymyles@gmail.com>
2023-12-10 01:41:28 -0800

    add find_by_id to job class

--- lib/models/job.py
+++ lib/models/job.py
@@ -147,6 +147,17 @@ class Job:
         rows = CURSOR.execute(sql).fetchall()
         return [cls.instance_from_db(row) for row in rows]
 
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM jobs
+            WHERE id = ?
+         """
+         row = CURSOR.execute(sql (id,)).fetchone()
+         return cls.instance_from_db(row) if row else None
+
     
 
         
