c19d466b169b8846b8fbf52c66de7e0e21074fe8
Ej Myles <ejelymyles@gmail.com>
2023-12-09 23:51:27 -0800

    add create_table & drop_table to candidate class

--- lib/models/candidate.py
+++ lib/models/candidate.py
@@ -69,3 +69,28 @@ class Candidate:
         else:
             raise ValueError(
                 "job_id must reference a job in the database")
+
+
+    
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS candidates (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            title TEXT,
+            location TEXT,
+            stage TEXT,
+            job_id INTEGER,
+            FOREIGN KEY (job_id) REFERENCES jobs(id))
+         """
+         CURSOR.execute(sql)
+         CONN.commit()
+    
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS candidates;
+         """
+         CURSOR.execute(sql)
+         CONN.commit()
